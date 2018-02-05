9c2ce843e56280cd9db169e7f3d313d8ec08560c
Lukas Hofwimmer <lukas.hofwimmer@gmail.com>
2018-02-05 08:54:08 +0100

    Added custom number of threads option in cfg.txt

--- md5check/Program.cs
+++ md5check/Program.cs
@@ -13,80 +13,38 @@ namespace md5check
     {
         static void Main(string[] args)
         {
-            md5result = File.ReadAllText(Environment.CurrentDirectory + "\\hash.txt");
+            var parameters = File.ReadAllLines(Environment.CurrentDirectory + "\\cfg.txt");
+            ProcessConfig(parameters);
             CheckforResult(md5result);
             Console.ReadKey();
         }
+
+        private static void ProcessConfig(string[] parameters)
+        {
+            md5result = parameters[0];
+            numThreads = int.Parse(parameters[1]);
+        }
+
         static string md5result;
+        static int numThreads;
 
 
         static List<string> fullArray;
 
-        static List<string> array1, array2, array3, array4, array5, array6, array7, array8, array9, array10, array11, array12, array13, array14;
         private static void CheckforResult(string md5result)
         {
             var path = $@"{Environment.CurrentDirectory}\wordlist.txt";
             var array = File.ReadAllLines(path).ToList();
             fullArray = array;
 
-            var arrlen = array.Count() / 14;
-            array1 = array.GetRange(0, arrlen);
-            array2 = array.GetRange(arrlen, arrlen);
-            array3 = array.GetRange(arrlen * 2, arrlen);
-            array4 = array.GetRange(arrlen * 3, arrlen);
-            array5 = array.GetRange(arrlen * 4, arrlen);
-            array6 = array.GetRange(arrlen * 5, arrlen);
-            array7 = array.GetRange(arrlen * 6, arrlen);
-            array8 = array.GetRange(arrlen * 7, arrlen);
-            array9 = array.GetRange(arrlen * 8, arrlen);
-            array10 = array.GetRange(arrlen * 9, arrlen);
-            array11 = array.GetRange(arrlen * 10, arrlen);
-            array12 = array.GetRange(arrlen * 11, arrlen);
-            array13 = array.GetRange(arrlen * 12, arrlen);
-            array14 = array.GetRange(arrlen * 13, arrlen);
 
-            var t1 = new Thread(() => RealStart(array1));
-            t1.Start();
+            var arrlen = array.Count() / numThreads;
+            for (int i=0;i<numThreads;i++)
+            {
+                var temparray = array.GetRange(arrlen*i,arrlen);
+                Task t = Task.Factory.StartNew(() => RealStart(temparray));   
 
-            var t2 = new Thread(() => RealStart(array2));
-            t2.Start();
-
-            var t3 = new Thread(() => RealStart(array3));
-            t3.Start();
-
-            var t4 = new Thread(() => RealStart(array4));
-            t4.Start();
-
-            var t5 = new Thread(() => RealStart(array5));
-            t5.Start();
-
-            var t6 = new Thread(() => RealStart(array6));
-            t6.Start();
-
-            var t7 = new Thread(() => RealStart(array7));
-            t7.Start();
-
-            var t8 = new Thread(() => RealStart(array8));
-            t8.Start();
-
-            var t9 = new Thread(() => RealStart(array9));
-            t9.Start();
-
-            var t10 = new Thread(() => RealStart(array10));
-            t10.Start();
-
-            var t11 = new Thread(() => RealStart(array11));
-            t11.Start();
-
-            var t12 = new Thread(() => RealStart(array12));
-            t12.Start();
-
-            var t13 = new Thread(() => RealStart(array13));
-            t13.Start();
-
-            var t14 = new Thread(() => RealStart(array14));
-            t14.Start();
-
+            }
             var countert = new Thread(() => PrintCounter());
             countert.Start();
         }
@@ -114,8 +72,6 @@ namespace md5check
                     var testword = array[i] + fullArray[j];
                     string result = CalculateMD5Hash(testword);
                     globalcounter++;
-
-                    
 
                     //Console.WriteLine(globalcounter + " | " + md5result);
                     if (result.Equals(md5result))
